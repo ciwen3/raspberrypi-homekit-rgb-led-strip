22359567f4c1b6033ca6547a97aaafd7ecfa349f
ciwen3 <55612276+ciwen3@users.noreply.github.com>
2022-10-22 14:07:59 -0700

    Create LED.py

---
+++ LED.py
@@ -0,0 +1,13 @@
+from gpiozero import LED
+from time import sleep
+
+led = LED(17)
+
+while True:
+    led.on()
+    print("LED ON")
+    sleep(1)
+    led.off()
+    print("LED OFF")
+    sleep(1)
+    
